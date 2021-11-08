6af2fd4a82c0861f08a21a2521d918d92a7cc230
첫 커밋 <>
2021-11-08 23:38:05 +0900

    [학력 제작중] [사진, 대학교 학력 부분 넣는 중]

--- src/components/body/About.vue
+++ src/components/body/About.vue
@@ -1,8 +1,47 @@
 <template>
   <v-main>
-   <v-row>
-     <v-col>
-       <v-img src="../../assets/shsin.jpg"></v-img>
+   <v-row justify="center" class="mt-4">
+     <v-col cols="1">
+
+     </v-col>
+     <v-col cols="1">
+       <v-card>
+        <v-img src="../../assets/IMG_2553.jpg"></v-img>
+       </v-card>
+     </v-col>
+     <v-col cols="2">
+      <div>
+        <h2>정연상</h2>
+        <p class="mt-3"><v-icon class="mr-3">mdi-cake</v-icon>1997.08.20</p>
+        <p><v-icon class="mr-3">mdi-email</v-icon>wjddustkd45@gmail.com</p>
+        <p><v-icon class="mr-3">mdi-cellphone</v-icon>010-8804-1072</p>
+      </div>
+     </v-col>
+   </v-row>
+   <v-row　justify="center">
+     <v-col cols="7">
+       <v-card>
+         <div class="text-center">
+           <h2>学歴</h2>
+         </div>
+         <hr>
+         <v-row class="mt-2" no-gutters>
+           <v-col cols="4">
+             <p>2013.03~2016.02</p>
+           </v-col>
+           <v-col cols="4">
+             <p>경복고등학교 졸업</p>
+           </v-col>
+         </v-row>
+         <v-row>
+           <v-col cols="4">
+             <p>2018.04~2019.06</p>
+           </v-col>
+           <v-col cols="3">
+             <p>芝浦工業大学土木工学科１年生修了</p>
+           </v-col>
+         </v-row>
+       </v-card>
      </v-col>
    </v-row>
   </v-main>
